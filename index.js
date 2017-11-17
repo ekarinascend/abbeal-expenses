c9359a53e5ee846f130127e00ebc967917737e64
Louis Larpin <louis.larpin@gmail.com>
2017-11-17 21:30:49 +0100

    log redux state in dev mode only

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import { Navigation } from 'react-native-navigation';
 import { Provider } from 'react-redux';
 import registerScreens from './src/screens';
-import configureStore from './src/store';
+import store from './src/store';
 
-const store = configureStore();
 registerScreens(store, Provider);
 
 Navigation.startSingleScreenApp({
--- src/store.js
+++ src/store.js
@@ -1,10 +1,13 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import logger from 'redux-logger';
 import reducers from './reducers';
 
-export default function configureStore() {
-  return createStore(
-    reducers,
-    applyMiddleware(logger),
-  );
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
 }
+
+const store = compose(applyMiddleware(...middlewares))(createStore)(reducers);
+
+export default store;
